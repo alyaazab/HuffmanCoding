783fa7130a95bf0a4169329090ec24c33393804e
Alya Azab <alya.113@live.com>
2019-12-11 23:35:27 +0200

    reading from file and adding to hashmap

--- src/com/company/FileUtils.java
+++ src/com/company/FileUtils.java
@@ -1,39 +1,49 @@
 package com.company;
 
 import java.io.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-public class FileMethods {
+public class FileUtils {
 
-    static int[] frequency = new int[128];
+    //create a hashmap that stores each character's ascii code and its frequency in the file
+    static HashMap<Integer, Integer> frequency = new HashMap<>();
 
-    public static void populateFrequencyArray(String filename) {
-        File file = new File(filename);
-        FileReader fileReader = null;
+    //create a priority queue that stores all hashmap entries, ordered in ascending frequency order
+    static PriorityQueue<HashMap.Entry<Integer, Integer>> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+
+
+    //this method takes in a file name
+    //reads the file character by character and populates the frequency hashmap and the priority queue
+    public static void calculateFrequencies(String filename) {
         try {
-            fileReader = new FileReader(file);
+            File file = new File(filename);
+            FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            int c = 0;
+            int c;
 
+            //read from file character by character, incrementing each character's frequency when it is encountered
             while((c = bufferedReader.read()) != -1)
             {
-//                char character = (char) c;
-                System.out.println(c);
-                frequency[c]++;
+                if(frequency.get(c) == null)
+                    frequency.put(c, 1);
+                else
+                    frequency.put(c, frequency.get(c)+1);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        printFrequencyArray();
+        //add all hashmap entries into priority queue
+        priorityQueue.addAll(frequency.entrySet());
 
+        //remove all elements from queue to show that they are removed in order of the smallest frequency
+        while(!priorityQueue.isEmpty())
+        {
+            System.out.println(priorityQueue.poll());
+        }
     }
-
-    public static void printFrequencyArray(){
-        for(int i=0; i<frequency.length; i++)
-            System.out.println((char)i + "\t" + frequency[i]);
-
-    }
-
-
 }
